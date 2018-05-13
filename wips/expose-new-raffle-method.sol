a3de37abad74c8906dcf4fe64ea2d4d7dafb616c
Greg Kapka <greg@kapka.co.uk>
2018-05-13 12:23:00 +0100

    write new makeRaffle func

--- wips/expose-new-raffle-method.sol
+++ wips/expose-new-raffle-method.sol
@@ -1,3 +1,31 @@
 /**
  * Need to expose the newRaffle() method externall & only callable by Etheraffle
  */
+
+ contract ExposeNewRaffle {
+
+   function manuallyMakeNewRaffle(uint _week, bool _paused) onlyEtheraffle {
+	   if (paused != _paused) paused = _paused; 
+	   week = _week;
+	   raffle[_week].timeStamp = BIRTHDAY + (_weel * WEEKDUR);
+   }
+
+}
+
+//  /**
+//  * @dev   Function which gets current week number and if different
+//  *        from the global var week number, it updates that and sets
+//  *        up the new raffle struct. Should only be called once a
+//  *        week after the raffle is closed. Should it get called
+//  *        sooner, the contract is paused for inspection.
+//  */
+// function newRaffle() internal {
+//     uint newWeek = getWeek();
+//     if (newWeek == week) {
+//         pauseContract(4);
+//         return;
+//     } else {//∴ new raffle...
+//         week = newWeek;
+//         raffle[newWeek].timeStamp = BIRTHDAY + (newWeek * WEEKDUR);
+//     }
+// }
