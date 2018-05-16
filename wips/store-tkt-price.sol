6762746ae7f17514e1641a0c498f9921ca17e287
Greg Kapka <greg@kapka.co.uk>
2018-05-16 14:05:54 +0100

    add change requirements

--- wips/store-tkt-price.sol
+++ wips/store-tkt-price.sol
@@ -1,6 +1,12 @@
 /**
  * Store tkt price in raffle struct to remedy the potential game theoretic ramifications 
  * or the tkt price changes w/r/t withdrawal time of a won prize?
+ *
+ * Need to reference it now in all tktPrice things!
+ * Need the new raffle to bake the tkt price in
+ * Need the ticket purchases to reference that price
+ * Need the withdraw to calculate prize based off that price.
+ * Front end won't matter since that references prizes based off array stored in db
  */
 
  contract StoreTktPrice {
@@ -15,6 +21,7 @@
         bool wdrawOpen;
         uint numEntries;
         uint freeEntries;
+        uint tktPrice;
     }
 
  }
