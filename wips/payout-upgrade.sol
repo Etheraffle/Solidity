eba791716148dd85dd8d788ac5954681c9513548
Greg Kapka <greg@kapka.co.uk>
2018-05-16 16:14:56 +0100

    add the payouts func to edit

--- wips/payout-upgrade.sol
+++ wips/payout-upgrade.sol
@@ -42,4 +42,45 @@ contract PayoutUpgrade {
     // function splitsSingle(uint _numWinners, uint _matchesIndex) internal view returns (uint) {
     //     return (prizePool * pctOfPool[_matchesIndex]) / (_numWinners * 1000);
     // }
+    /**
+     * @dev   Takes the results of the oraclize Etheraffle api call back
+     *        and uses them to calculate the prizes due to each tier
+     *        (3 matches, 4 matches etc) then pushes them into the winning
+     *        amounts array in the raffle in question's struct. Calculates
+     *        the total winnings of the raffle, subtracts it from the
+     *        global prize pool sequesters that amount into the raffle's
+     *        struct "unclaimed" variable, ∴ "rolling over" the unwon
+     *        ether. Enables winner withdrawals by setting the withdraw
+     *        open bool to true.
+     *
+     * @param _week    The week number of the raffle in question.
+     * @param _result  The results string from oraclize callback.
+     */
+    function setPayOuts(uint _week, string _result) internal {
+        string[] memory numWinnersStr = stringToArray(_result);
+        if (numWinnersStr.length < 4) return pauseContract(2);
+        uint[] memory numWinnersInt = new uint[](4);
+        for (uint i = 0; i < 4; i++) {
+            numWinnersInt[i] = parseInt(numWinnersStr[i]);
+        }
+        uint[] memory payOuts = new uint[](4);
+        uint total;
+        for (i = 0; i < 4; i++) {
+            if (numWinnersInt[i] != 0) {
+                uint amt = oddsTotal(numWinnersInt[i], i) <= splitsTotal(numWinnersInt[i], i) 
+                         ? oddsSingle(i) 
+                         : splitsSingle(numWinnersInt[i], i); 
+                payOuts[i] = amt;
+                total += payOuts[i] * numWinnersInt[i];
+            }
+        }
+        raffle[_week].unclaimed = total;
+        if (raffle[_week].unclaimed > prizePool) return pauseContract(3);
+        prizePool -= raffle[_week].unclaimed;
+        for (i = 0; i < payOuts.length; i++) {
+            raffle[_week].winAmts.push(payOuts[i]);
+        }
+        raffle[_week].wdrawOpen = true;
+        emit LogPrizePoolsUpdated(prizePool, _week, raffle[_week].unclaimed, payOuts[0], payOuts[1], payOuts[2], payOuts[3], now);
+    }
 }
