8f27284c74177f0d01237f4abcb6f7c44d5fd2e4
Greg Kapka <greg@kapka.co.uk>
2018-05-16 14:01:34 +0100

    init commit

--- wips/store-tkt-price.sol
+++ wips/store-tkt-price.sol
@@ -2,3 +2,19 @@
  * Store tkt price in raffle struct to remedy the potential game theoretic ramifications 
  * or the tkt price changes w/r/t withdrawal time of a won prize?
  */
+
+ contract StoreTktPrice {
+
+    mapping (uint => rafStruct) public raffle;
+    struct rafStruct {
+        mapping (address => uint[][]) entries;
+        uint unclaimed;
+        uint[] winNums;
+        uint[] winAmts;
+        uint timeStamp;
+        bool wdrawOpen;
+        uint numEntries;
+        uint freeEntries;
+    }
+
+ }
