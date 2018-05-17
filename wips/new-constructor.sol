31938350db86b9fa5ef17b432a5881f9dd25b544
Greg Kapka <greg@kapka.co.uk>
2018-05-17 17:21:35 +0100

    proposed new constructor

--- wips/new-constructor.sol
+++ wips/new-constructor.sol
@@ -14,6 +14,19 @@ contract NewConstructor {
      * @param _msig       The address of the Etheraffle managerial multisig wallet.
      * @param _ethRelief  The address of the EthRelief charity contract.
      */
+    constructor(address _freeLOT, address _dsbrs, address _msig, address _ethRelief) payable {
+        week         = getWeek();
+        etheraffle   = _msig;
+        disburseAddr = _dsbrs;
+        ethRelief    = _ethRelief;
+        freeLOT      = FreeLOTInterface(_freeLOT);
+        // uint delay   = (week * WEEKDUR) + BIRTHDAY + rafEnd + resultsDelay;
+        raffle[week].timeStamp = (week * WEEKDUR) + BIRTHDAY;
+        // bytes32 query = oraclize_query(delay, "nested", strConcat(randomStr1, uint2str(getWeek()), randomStr2), gasAmt);
+        // qID[query].weekNo = week;
+        // qID[query].isRandom = true;
+        // emit LogQuerySent(query, delay, now);
+    }
 }
 
 
