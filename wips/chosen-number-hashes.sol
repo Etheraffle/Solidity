55d6f2a19a23789289a64eb30f01b54bae7115f3
Greg Kapka <greg@kapka.co.uk>
2018-05-19 12:49:39 +0100

    add wDraw func for refactoring

--- wips/chosen-number-hashes.sol
+++ wips/chosen-number-hashes.sol
@@ -113,5 +113,42 @@
         raffle[week].entries[_entrant].push(keccak256(_cNums);
         emit LogTicketBought(week, raffle[week].numEntries, _entrant, _cNums, raffle[week].entries[_entrant].length, _value, now, _affID);
     }
-
+    /**
+     * @dev Withdraw Winnings function. User calls this function in order to withdraw
+     *      whatever winnings they are owed. Function can be paused via the modifier
+     *      function "onlyIfNotPaused"
+     *
+     * @param _week        Week number of the raffle the winning entry is from
+     * @param _entryNum    The entrants entry number into this raffle
+     */
+    function withdrawWinnings(uint _week, uint _entryNum) onlyIfNotPaused external {
+        require
+        (
+            raffle[_week].timeStamp > 0 &&
+            now - raffle[_week].timeStamp > WEEKDUR - (WEEKDUR / 7) &&
+            now - raffle[_week].timeStamp < wdrawBfr &&
+            raffle[_week].wdrawOpen == true &&
+            raffle[_week].entries[msg.sender][_entryNum - 1].length == 6
+        );
+        uint matches = getMatches(_week, msg.sender, _entryNum);
+        if (matches == 2) return winFreeGo(_week, _entryNum);
+        require
+        (
+            matches >= 3 &&
+            raffle[_week].winAmts[matches - 3] > 0 &&
+            raffle[_week].winAmts[matches - 3] <= this.balance
+        );
+        raffle[_week].entries[msg.sender][_entryNum - 1].push(1);
+        if (raffle[_week].winAmts[matches - 3] <= raffle[_week].unclaimed) {
+            raffle[_week].unclaimed -= raffle[_week].winAmts[matches - 3];
+        } else {
+            raffle[_week].unclaimed = 0;
+            pauseContract(5);
+        }
+        msg.sender.transfer(raffle[_week].winAmts[matches - 3]);
+        emit LogWithdraw(_week, msg.sender, _entryNum, matches, raffle[_week].winAmts[matches - 3], now);
+    }
  }
+
+     
+
