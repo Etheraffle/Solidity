22c78ac580f36cfe358887ea752923933eb798ca
Greg Kapka <greg@kapka.co.uk>
2018-05-19 23:45:31 +0100

    add in getMatches func to refactor

--- wips/chosen-number-hashes.sol
+++ wips/chosen-number-hashes.sol
@@ -168,6 +168,28 @@
     // function isAlreadyWithdrawn(uint _week, uint _entryNum, uint[] _cNums, _entrant) view internal returns (bool) {
     //     return raffle[_week].entries[_entrant][_entryNum - 1] == keccak256(_cNums, 0);
     // }
+
+        /**
+     * @dev   Function compares array of entrant's 6 chosen numbers to
+      *       the raffle in question's winning numbers, counting how
+      *       many matches there are.
+      *
+      * @param _week         The week number of the Raffle in question
+      * @param _entrant      Entrant's ethereum address
+      * @param _entryNum     number of entrant's entry in question.
+     */
+    function getMatches(uint _week, address _entrant, uint _entryNum) constant internal returns (uint) {
+        uint matches;
+        for (uint i = 0; i < 6; i++) {
+            for (uint j = 0; j < 6; j++) {
+                if (raffle[_week].entries[_entrant][_entryNum - 1][i] == raffle[_week].winNums[j]) {
+                    matches++;
+                    break;
+                }
+            }
+        }
+        return matches;
+    }
  }
 
      
