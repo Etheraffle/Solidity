f8b0898e99d37b64521aeb9aaae78d14cdfbb9b7
Greg Kapka <greg@kapka.co.uk>
2018-05-24 18:13:10 +0100

    add NATSPEC comment

--- wips/overloaded-pause-function.sol
+++ wips/overloaded-pause-function.sol
@@ -1,7 +1,7 @@
 //WRITE OVERLOADED SET PAUSE FUNCTION, ONE WITHOUT THE ID PARAM AND ONE WITH?!?!?
 
     /**
-     * @dev  To pause the contract's functions should the need arise. Internal.
+     * @dev  To pause the contract's functions should the need arise.
      *       Logs an event of the pausing.
      *
      * @param _id    A uint to identify the caller of this function.
@@ -10,7 +10,14 @@
       paused = _status;
       emit LogFunctionsPaused(_id, now);
     }
-
+    /**
+     * @dev     Sets the paused status of the contract to the bool 
+     *          passed in. This affects various of the contracts 
+     *          functions via the onlyIfNotPaused modifier.
+     *
+     * @param   _status     Desired pause status.
+     *
+     */
     function pauseContract(bool _status) internal {
       paused = _status;
     }
