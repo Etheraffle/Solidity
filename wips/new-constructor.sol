cf25d642a3c53d17de1ca68bdfcf084bac124e3e
Greg Kapka <greg@kapka.co.uk>
2018-05-24 13:54:37 +0100

    rm commented out code and fix NATSPEC spacing

--- wips/new-constructor.sol
+++ wips/new-constructor.sol
@@ -1,21 +1,21 @@
 contract NewConstructor {
     /**
-     * @dev   Constructor - sets the Etheraffle contract address &
-     *        the disbursal contract address for investors, calls
-     *        the newRaffle() function with sets the current
-     *        raffle ID global var plus sets up the first raffle's
-     *        struct with correct time stamp. Sets the withdraw
-     *        before time to a ten week period, and prepares the
-     *        initial oraclize call which will begin the recursive
-     *        function.
+     * @dev     Constructor - sets the Etheraffle contract address &
+     *          the disbursal contract address for investors, calls
+     *          the newRaffle() function with sets the current
+     *          raffle ID global var plus sets up the first raffle's
+     *          struct with correct time stamp. Sets the withdraw
+     *          before time to a ten week period, and prepares the
+     *          initial oraclize call which will begin the recursive
+     *          function.
      *
-     * @param _freeLOT    The address of the Etheraffle FreeLOT special token.
+     * @param   _freeLOT    The address of the Etheraffle FreeLOT special token.
      *
-     * @param _dsbrs      The address of the Etheraffle disbursal contract.
+     * @param   _dsbrs      The address of the Etheraffle disbursal contract.
      *
-     * @param _msig       The address of the Etheraffle managerial multisig wallet.
+     * @param   _msig       The address of the Etheraffle multisig wallet.
      *
-     * @param _ethRelief  The address of the EthRelief charity contract.
+     * @param   _ethRelief  The address of the EthRelief charity contract.
      *
      */
     constructor(address _freeLOT, address _dsbrs, address _msig, address _ethRelief) payable {
@@ -25,12 +25,5 @@ contract NewConstructor {
         ethRelief    = _ethRelief;
         freeLOT      = FreeLOTInterface(_freeLOT);
         setupRaffleStruct(week, 2500000000000000, (week * WEEKDUR) + BIRTHDAY);
-        // raffle[week].timeStamp = (week * WEEKDUR) + BIRTHDAY;
-        // raffle[week].tktPrice = 2500000000000000;
-        // uint delay   = (week * WEEKDUR) + BIRTHDAY + rafEnd + resultsDelay;
-        // bytes32 query = oraclize_query(delay, "nested", strConcat(randomStr1, uint2str(getWeek()), randomStr2), gasAmt);
-        // qID[query].weekNo = week;
-        // qID[query].isRandom = true;
-        // emit LogQuerySent(query, delay, now);
     }
 }
