3ee94dcc38ae5fc3610ac3108c01909bb9580f19
Greg Kapka <greg@kapka.co.uk>
2018-05-25 23:33:07 +0100

    add disbursal func for upgrading

---
+++ wips/new-disburse-funds.sol
@@ -0,0 +1,26 @@
+    /**
+     * @dev  Function totals up oraclize cost for the raffle, subtracts
+     *       it from the prizepool (if less than, if greater than if
+     *       pauses the contract and fires an event). Calculates profit
+     *       based on raffle's tickets sales and the take percentage,
+     *       then forwards that amount of ether to the disbursal contract.
+     *
+     * @param _week   The week number of the raffle in question.
+     */
+    function disburseFunds(uint _week) internal {
+        uint oracTot = 2 * ((gasAmt * gasPrc) + oracCost);//2 queries per draw...
+        if (oracTot > prizePool) return pauseContract(1);
+        prizePool -= oracTot;
+        uint profit;
+        if (raffle[_week].numEntries > 0) {
+            profit = ((raffle[_week].numEntries - raffle[_week].freeEntries) * tktPrice * take) / 1000;
+            prizePool -= profit;
+            uint half = profit / 2;
+            ReceiverInterface(disburseAddr).receiveEther.value(half)();
+            ReceiverInterface(ethRelief).receiveEther.value(profit - half)();
+            emit LogFundsDisbursed(_week, oracTot, profit - half, ethRelief, now);
+            emit LogFundsDisbursed(_week, oracTot, half, disburseAddr, now);
+            return;
+        }
+        emit LogFundsDisbursed(_week, oracTot, profit, 0, now);
+    }
