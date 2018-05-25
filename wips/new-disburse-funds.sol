fcb57dc09cba09ed7b5a08df86b32249037525f8
Greg Kapka <greg@kapka.co.uk>
2018-05-25 23:42:28 +0100

    rename variable for more clarity

--- wips/new-disburse-funds.sol
+++ wips/new-disburse-funds.sol
@@ -8,9 +8,9 @@
      * @param _week   The week number of the raffle in question.
      */
     function disburseFunds(uint _week) internal {
-        uint oracTot = getOraclizeCost();
-        if (oracTot > prizePool) return pauseContract(true, 1);
-        prizePool -= oracTot;
+        uint cost = getOraclizeCost();
+        if (cost > prizePool) return pauseContract(true, 1);
+        prizePool -= cost;
         uint profit;
         if (raffle[_week].numEntries > 0) {
             profit = ((raffle[_week].numEntries - raffle[_week].freeEntries) * tktPrice * take) / 1000;
@@ -18,11 +18,11 @@
             uint half = profit / 2;
             ReceiverInterface(disburseAddr).receiveEther.value(half)();
             ReceiverInterface(ethRelief).receiveEther.value(profit - half)();
-            emit LogFundsDisbursed(_week, oracTot, profit - half, ethRelief, now);
-            emit LogFundsDisbursed(_week, oracTot, half, disburseAddr, now);
+            emit LogFundsDisbursed(_week, cost, profit - half, ethRelief, now);
+            emit LogFundsDisbursed(_week, cost, half, disburseAddr, now);
             return;
         }
-        emit LogFundsDisbursed(_week, oracTot, profit, 0, now);
+        emit LogFundsDisbursed(_week, cost, profit, 0, now);
     }
     /**
      * @dev     Returns the cost of the Oraclize api calls
