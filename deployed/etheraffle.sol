c74d639c5e101da120a67c317953ca5c3ed8ac35
Greg Kapka <greg@kapka.co.uk>
2018-05-26 22:41:01 +0100

    rm redundant getMatches func

--- deployed/etheraffle.sol
+++ deployed/etheraffle.sol
@@ -873,27 +873,6 @@ contract Etheraffle is usingOraclize {
         emit LogPrizePoolsUpdated(prizePool, _week, raffle[_week].unclaimed, payOuts[0], payOuts[1], payOuts[2], payOuts[3], now);
     }
     /**
-     * @dev   Function compares array of entrant's 6 chosen numbers to
-      *       the raffle in question's winning numbers, counting how
-      *       many matches there are.
-      *
-      * @param _week         The week number of the Raffle in question
-      * @param _entrant      Entrant's ethereum address
-      * @param _entryNum     number of entrant's entry in question.
-     */
-    function getMatches(uint _week, address _entrant, uint _entryNum) constant internal returns (uint) {
-        uint matches;
-        for (uint i = 0; i < 6; i++) {
-            for (uint j = 0; j < 6; j++) {
-                if (raffle[_week].entries[_entrant][_entryNum - 1][i] == raffle[_week].winNums[j]) {
-                    matches++;
-                    break;
-                }
-            }
-        }
-        return matches;
-    }
-    /**
      * @dev     Manually make an Oraclize API call, incase of automation
      *          failure. Only callable by the Etheraffle address.
      *
