84c3c94277ef454bad160b5db4e9818a3eaebac5
Greg Kapka <greg@kapka.co.uk>
2018-05-26 14:38:38 +0100

    loop only 6 times and fix NATSPEC

--- wips/oraclize-callback-update.sol
+++ wips/oraclize-callback-update.sol
@@ -247,19 +247,21 @@ contract OraclizeUpdate {
         qID[_ID].isManual  = _isManual;
     }
     /**
-     * @dev   Takes oraclize random.org api call result string and splits
-     *        it at the commas into an array, parses those strings in that
-     *        array as integers and pushes them into the winning numbers
-     *        array in the raffle's struct. Fires event logging the data,
-     *        including the serial number of the random.org callback so
-     *        its veracity can be proven.
+     * @dev     Takes oraclize random.org api call result string and splits
+     *          it at the commas into an array, parses those strings in that
+     *          array as integers and pushes them into the winning numbers
+     *          array in the raffle's struct. Fires event logging the data,
+     *          including the serial number of the random.org callback so
+     *          its veracity can be proven.
      *
-     * @param _week    The week number of the raffle in question.
-     * @param _result   The results string from oraclize callback.
+     * @param   _week    The week number of the raffle in question.
+     *
+     * @param   _result   The results string from oraclize callback.
+     *
      */
     function setWinningNumbers(uint _week, string _result) internal {
         string[] memory arr = stringToArray(_result);
-        for (uint i = 0; i < arr.length; i++){
+        for (uint i = 0; i < 6; i++){
             raffle[_week].winNums.push(parseInt(arr[i]));
         }
         uint serialNo = parseInt(arr[6]);
