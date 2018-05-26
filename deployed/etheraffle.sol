7c3a910b878983e5b99197322de02dc356462fa7
Greg Kapka <greg@kapka.co.uk>
2018-05-26 22:30:22 +0100

    switch out recalim unclaimed for new versions

--- deployed/etheraffle.sol
+++ deployed/etheraffle.sol
@@ -657,24 +657,35 @@ contract Etheraffle is usingOraclize {
      *
      *      ##########################################
      *      ###                                    ###
-     *      ###         ?????????????????          ###
+     *      ###         Oraclize Callback          ###
      *      ###                                    ###
      *      ##########################################
      *
      */
-
-
-
     /**
-     * @dev    Called by the weekly oraclize callback. Checks raffle 10
+     * @dev    Called by the weekly Oraclize callback. Checks raffle 10
      *         weeks older than current raffle for any unclaimed prize
      *         pool. If any found, returns it to the main prizePool and
      *         zeros the amount.
+     *
      */
     function reclaimUnclaimed() internal {
         uint old = getWeek() - 11;
-        prizePool += raffle[old].unclaimed;
-        emit LogReclaim(old, raffle[old].unclaimed, now);
+        uint amt = getUnclaimed(old);
+        if (amt == 0) return;
+        modifyPrizePool(true, amt);
+        modifyUnclaimed(false, old, amt);
+        emit LogReclaim(old, amt, now);
+    }
+    /**
+     * @dev     Returns the unclaimed prize pool sequestered in a raffle's
+     *          struct.
+     *
+     * @param   _week   Week number for raffle in question.
+     *
+     */
+    function getUnclaimed(uint _week) public view returns (uint) {
+        return raffle[_week].unclaimed;
     }
     /**
      * @dev  Function totals up oraclize cost for the raffle, subtracts
