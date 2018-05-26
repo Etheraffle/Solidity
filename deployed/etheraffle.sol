831fc2f171b37036a4077c5d8a9f8245aa4b430e
Greg Kapka <greg@kapka.co.uk>
2018-05-26 22:53:30 +0100

    add in set winning numbers func

--- deployed/etheraffle.sol
+++ deployed/etheraffle.sol
@@ -812,6 +812,27 @@ contract Etheraffle is usingOraclize {
         return raffle[_week].winAmts.length > 0;
     }
     /**
+     * @dev     Takes oraclize random.org api call result string and splits
+     *          it at the commas into an array, parses those strings in that
+     *          array as integers and pushes them into the winning numbers
+     *          array in the raffle's struct. Fires event logging the data,
+     *          including the serial number of the random.org callback so
+     *          its veracity can be proven.
+     *
+     * @param   _week    The week number of the raffle in question.
+     *
+     * @param   _result   The results string from oraclize callback.
+     *
+     */
+    function setWinningNumbers(uint _week, string _result) internal {
+        string[] memory arr = stringToArray(_result);
+        for (uint i = 0; i < 6; i++){
+            raffle[_week].winNums.push(parseInt(arr[i]));
+        }
+        uint serialNo = parseInt(arr[6]);
+        emit LogWinningNumbers(_week, raffle[_week].numEntries, raffle[_week].winNums, prizePool, serialNo, now);
+    }
+    /**
      * @dev     Returns the number of seconds until the next occurring 
      *          raffle deadline.
      *
