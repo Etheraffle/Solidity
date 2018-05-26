74046e05172d581b1411f64a2bf37a3c27f823f4
Greg Kapka <greg@kapka.co.uk>
2018-05-26 14:39:53 +0100

    fix NATSPEC comment layout

---
+++ wips/getters.sol
@@ -0,0 +1,35 @@
+    /**
+     * @dev     Get a entrant's number of entries into a specific raffle
+     *
+     * @param   _week       The week number of the queried raffle.
+     *
+     * @param   _entrant    The entrant in question.
+     *
+     */
+    function getUserNumEntries(address _entrant, uint _week) constant external returns (uint) {
+        return raffle[_week].entries[_entrant].length;
+    }
+    /**
+     * @dev     Get chosen numbers of an entrant, for a specific raffle.
+     *          Returns an array.
+     *
+     * @param   _entrant    The entrant in question's address.
+     *
+     * @param   _week       The week number of the queried raffle.
+     *
+     * @param   _entryNum   The entrant's entry number in this raffle.
+     *
+     */
+    function getChosenNumbers(address _entrant, uint _week, uint _entryNum) constant external returns (uint[]) {
+        return raffle[_week].entries[_entrant][_entryNum-1];
+    }
+    /**
+     * @dev     Get winning details of a raffle, ie, it's winning numbers
+     *          and the prize amounts. Returns two arrays.
+     *
+     * @param   _week   The week number of the raffle in question.
+     *
+     */
+    function getWinningDetails(uint _week) constant external returns (uint[], uint[]) {
+        return (raffle[_week].winNums, raffle[_week].winAmts);
+    }
