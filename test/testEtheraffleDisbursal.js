5fb23ffdf1d6adfedb8e43d5afabfdbc635ea54b
Greg Kapka <greg@kapka.co.uk>
2018-06-06 22:07:05 +0100

    Add owner upgrade test

--- test/testEtheraffleDisbursal.js
+++ test/testEtheraffleDisbursal.js
@@ -1,6 +1,7 @@
 const { assert }    = require("chai")
-// , web3Abi       = require('web3-eth-abi')
+    // , web3Abi       = require('web3-eth-abi')
     , truffleAssert = require('truffle-assertions')
+    , receiver      = artifacts.require('etherReceiverStub')
     , disbursal     = artifacts.require('etheraffleDisbursal')
 
 contract('etheraffleDisbursal', accounts => {
@@ -26,12 +27,23 @@ contract('etheraffleDisbursal', accounts => {
     await contract.setEtheraffle(accounts[0], {from: accounts[1]})
     owner = await contract.etheraffle.call()
     assert.equal(owner, accounts[0])
-    // truffleAssert.eventEmitted(change1, 'LogEtheraffleChange', ev => 
-    //   ev.prevER == accounts[0] && ev.newER == accounts[1]
-    // )
-    // truffleAssert.eventEmitted(change2, 'LogEtheraffleChange', ev => 
-    //   ev.prevER == accounts[1] && ev.newER == accounts[0]
-    // )
+  })
+
+  it('Only owner can upgrade', async () => {
+    const contract = await disbursal.deployed()
+        , stub     = await receiver.deployed()
+        , owner    = await contract.etheraffle.call()
+    try {
+      await contract.upgrade(stub.address, {from: accounts[9]})
+      assert.fail('Non-owner should not be able to upgade contract!')
+    } catch (e) {
+      // console.log('Error in non-owner upgrading: ', e)
+      // Transaction failed as expected!
+    }
+    const upgrade = await contract.upgrade(stub.address, {from: owner})
+    truffleAssert.eventEmitted(upgrade, 'LogUpgrade', ev => 
+      ev.toWhere == stub.address && ev.amountTransferred.toNumber() == 0
+    )
   })
   
 })
