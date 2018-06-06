f5bf95db78aef8b511d4069fa6123874e09e7e40
Greg Kapka <greg@kapka.co.uk>
2018-06-06 13:28:42 +0100

    add LOT to migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
-const amt     = 100 // Init minting of the token 
-    , freeLOT = artifacts.require("EtheraffleFreeLOT")
-    , addr    = '0x627306090abab3a6e1400e9345bc60c78a8bef57' // Truffle's account[0]
+const etheraffleLOT     = artifacts.require("EtheraffleLOT")
+    , etheraffleFreeLOT = artifacts.require("EtheraffleFreeLOT")
+    , amt  = 100 // Init minting of the token for tests
+    , addr = '0x627306090abab3a6e1400e9345bc60c78a8bef57' // Truffle's account[0]
 
 module.exports = deployer => {
-  deployer.deploy(freeLOT, addr, amt)
+  deployer.deploy(etheraffleLOT, addr, amt)
+  deployer.deploy(etheraffleFreeLOT, addr, amt)
 }
