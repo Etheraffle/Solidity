22080c6c31738a98513db6827270e76479cf977d
Greg Kapka <greg@kapka.co.uk>
2018-06-06 17:26:26 +0100

    add ownership test

--- test/testEtheraffleDisbursal.js
+++ test/testEtheraffleDisbursal.js
@@ -1,14 +1,14 @@
 const { assert }    = require("chai")
 // , web3Abi       = require('web3-eth-abi')
     , truffleAssert = require('truffle-assertions')
-    , FreeLOT       = artifacts.require('etheraffleDisbursal')
+    , disbursal     = artifacts.require('etheraffleDisbursal')
 
 contract('etheraffleDisbursal', accounts => {
   
-  // it('Contract should be owned by account[0]', async () => {
-  //   const contract  = await FreeLOT.deployed()
-  //       , contOwner = await contract.etheraffle.call()
-  //   assert.equal(contOwner, accounts[0])
-  // })
+  it('Contract should be owned by account[0]', async () => {
+    const contract  = await disbursal.deployed()
+        , contOwner = await contract.etheraffle.call()
+    assert.equal(contOwner, accounts[0])
+  })
   
 })
