863f6f9292261d22560090e7eec921499de77f67
Greg Kapka <greg@kapka.co.uk>
2018-06-06 12:33:06 +0100

    rewrite migrations to be less gross

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,7 @@
-// const freeLOT = artifacts.require("./etheraffleFreeLOT.sol")
+const amt     = 100
+    , freeLOT = artifacts.require("EtheraffleFreeLOT")
+    , addr    = '0x627306090abab3a6e1400e9345bc60c78a8bef57'
 
-// /* Constructor Args */
-// const amt  = 100
-// const addr = '0x627306090abab3a6e1400e9345bc60c78a8bef57'
-
-// module.exports = deployer => {
-//   deployer.deploy(freeLOT, addr, amt);
-// }
-var freeLOT = artifacts.require("EtheraffleFreeLOT");
-// var MetaCoin = artifacts.require("./MetaCoin.sol");
-var amt  = 100
-var addr = '0x627306090abab3a6e1400e9345bc60c78a8bef57'
-
-module.exports = function(deployer) {
-  deployer.deploy(freeLOT, addr, amt);
-};
+module.exports = deployer => {
+  deployer.deploy(freeLOT, addr, amt)
+}
