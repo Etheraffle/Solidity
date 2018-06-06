f9f6485c3ccc39e0f2d02498385a46145522f337
Greg Kapka <greg@kapka.co.uk>
2018-06-06 17:00:48 +0100

    set up test files for disbursal contract

---
+++ test/testEtheraffleDisbursal.js
@@ -0,0 +1,14 @@
+const { assert }    = require("chai")
+// , web3Abi       = require('web3-eth-abi')
+    , truffleAssert = require('truffle-assertions')
+    , FreeLOT       = artifacts.require('etheraffleDisbursal')
+
+contract('etheraffleDisbursal', accounts => {
+  
+  // it('Contract should be owned by account[0]', async () => {
+  //   const contract  = await FreeLOT.deployed()
+  //       , contOwner = await contract.etheraffle.call()
+  //   assert.equal(contOwner, accounts[0])
+  // })
+  
+})
