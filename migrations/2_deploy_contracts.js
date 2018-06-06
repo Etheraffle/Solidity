f416833d22aaa583306e8cb0c97acc3ae97d16ce
Greg Kapka <greg@kapka.co.uk>
2018-06-06 12:33:46 +0100

    add clarifying notes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
-const amt     = 100
+const amt     = 100 // Init minting of the token 
     , freeLOT = artifacts.require("EtheraffleFreeLOT")
-    , addr    = '0x627306090abab3a6e1400e9345bc60c78a8bef57'
+    , addr    = '0x627306090abab3a6e1400e9345bc60c78a8bef57' // Truffle's account[0]
 
 module.exports = deployer => {
   deployer.deploy(freeLOT, addr, amt)
