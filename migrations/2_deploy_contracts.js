21f5bdd07868f1cbccf89b580289c785966ef3bc
Greg Kapka <greg@kapka.co.uk>
2018-06-06 23:01:05 +0100

    add new contract into deploy pipeline

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
-const etheraffleLOT       = artifacts.require("EtheraffleLOT")
+const ethRelief           = artifacts.require("EthRelief")
+    , etheraffleLOT       = artifacts.require("EtheraffleLOT")
     , etheraffleFreeLOT   = artifacts.require("EtheraffleFreeLOT")
-    , etheraffleDisbursal = artifacts.require("EtheraffleDisbursal")
     , etherReceiverStub   = artifacts.require("EtherReceiverStub")
+    , etheraffleDisbursal = artifacts.require("EtheraffleDisbursal")
     , amt  = 100 // Init minting of the token for tests
     , addr = '0x627306090abab3a6e1400e9345bc60c78a8bef57' // Truffle's account[0]
 
 module.exports = deployer => {
+  deployer.deploy(ethRelief, addr)
   deployer.deploy(etherReceiverStub, addr)
   deployer.deploy(etheraffleLOT, addr, amt)
   deployer.deploy(etheraffleDisbursal, addr)
