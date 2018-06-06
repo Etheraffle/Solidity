2566d1d7ac7d315a3d4deedd2f3dee9a00781c1f
Greg Kapka <greg@kapka.co.uk>
2018-06-06 16:59:10 +0100

    add disbursal to deploys

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
-const etheraffleLOT     = artifacts.require("EtheraffleLOT")
-    , etheraffleFreeLOT = artifacts.require("EtheraffleFreeLOT")
+const etheraffleLOT       = artifacts.require("EtheraffleLOT")
+    , etheraffleFreeLOT   = artifacts.require("EtheraffleFreeLOT")
+    , etheraffleDisbursal = artifacts.require("EtheraffleDisbursal")
     , amt  = 100 // Init minting of the token for tests
     , addr = '0x627306090abab3a6e1400e9345bc60c78a8bef57' // Truffle's account[0]
 
 module.exports = deployer => {
   deployer.deploy(etheraffleLOT, addr, amt)
+  deployer.deploy(etheraffleDisbursal, addr)
   deployer.deploy(etheraffleFreeLOT, addr, amt)
 }
