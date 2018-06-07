44007e6270725f2afd088e65bed386ae8b835d9e
Greg Kapka <greg@kapka.co.uk>
2018-06-07 20:35:18 +0100

    add deploy promise chain for ER contract deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,9 +7,18 @@ const ethRelief           = artifacts.require("EthRelief")
     , addr = '0x627306090abab3a6e1400e9345bc60c78a8bef57' // Truffle's account[0]
 
 module.exports = deployer => {
-  deployer.deploy(ethRelief, addr)
   deployer.deploy(etherReceiverStub, addr)
+  deployer.deploy(ethRelief, addr)
   deployer.deploy(etheraffleLOT, addr, amt)
   deployer.deploy(etheraffleDisbursal, addr)
   deployer.deploy(etheraffleFreeLOT, addr, amt)
+  // deployer.deploy(ethRelief, addr).then(() => {
+  //   return deployer.deploy(etheraffleDisbursal, addr).then(() => {
+  //     return deployer.deploy(etheraffleFreeLOT, addr, amt).then(() => {
+  //       //Now have access to the above contract addresses for deploying ER.
+  //       return deployer.deploy(etheraffle, addr, etheraffleDisbursal.address, ethRelief.address, etheraffleFreeLOT.address)
+  //        Once ER compiles, this alone might be enough for testing since it's deploying them all...
+  //     })
+  //   })
+  // })
 }
