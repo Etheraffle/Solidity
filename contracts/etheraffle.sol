ba2c8906297a72c9a4b7ce8ef971b0850480e973
Greg Kapka <greg@kapka.co.uk>
2018-06-07 22:16:26 +0100

    add missing semicolon

--- contracts/etheraffle.sol
+++ contracts/etheraffle.sol
@@ -220,7 +220,7 @@ contract Etheraffle is usingOraclize {
         freeLOT      = FreeLOTInterface(_freeLOT);
         setUpRaffleStruct(week, 2500000000000000, (week * WEEKDUR) + BIRTHDAY);
         // TODO: Remove in prod!
-        OAR = OraclizeAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475)
+        OAR = OraclizeAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475);
     }
     /**
      *
