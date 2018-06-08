87ebcb85386f53c5ce33eb5f26ec1cf889b82448
Greg Kapka <greg@kapka.co.uk>
2018-06-08 15:19:23 +0100

    organise deployment order to pass correct args to ER

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,24 +1,20 @@
 const ethRelief           = artifacts.require("EthRelief")
+    , etheraffle          = artifacts.require("Etheraffle")
     , etheraffleLOT       = artifacts.require("EtheraffleLOT")
     , etheraffleFreeLOT   = artifacts.require("EtheraffleFreeLOT")
     , etherReceiverStub   = artifacts.require("EtherReceiverStub")
     , etheraffleDisbursal = artifacts.require("EtheraffleDisbursal")
-    , amt  = 100 // Init minting of the token for tests
-    , addr = '0x627306090abab3a6e1400e9345bc60c78a8bef57' // Truffle's account[0]
 
-module.exports = deployer => {
+module.exports = (deployer, networks, accounts) => {
+  const amt  = 100
+      , addr = accounts[0]
   deployer.deploy(etherReceiverStub, addr)
-  deployer.deploy(ethRelief, addr)
   deployer.deploy(etheraffleLOT, addr, amt)
-  deployer.deploy(etheraffleDisbursal, addr)
-  deployer.deploy(etheraffleFreeLOT, addr, amt)
-  // deployer.deploy(ethRelief, addr).then(() => {
-  //   return deployer.deploy(etheraffleDisbursal, addr).then(() => {
-  //     return deployer.deploy(etheraffleFreeLOT, addr, amt).then(() => {
-  //       //Now have access to the above contract addresses for deploying ER.
-  //       return deployer.deploy(etheraffle, addr, etheraffleDisbursal.address, ethRelief.address, etheraffleFreeLOT.address)
-  //        Once ER compiles, this alone might be enough for testing since it's deploying them all...
-  //     })
-  //   })
-  // })
+  deployer.deploy(ethRelief, addr).then(ethReliefRes =>
+    deployer.deploy(etheraffleDisbursal, addr).then(disbRes =>
+      deployer.deploy(etheraffleFreeLOT, addr, amt).then(freeAddr =>
+        deployer.deploy(etheraffle, addr, disbRes.address, ethReliefRes.address, freeAddr.address, {value: 1*10**17, gas: 6700000})
+      )
+    )
+  )
 }
