49c1d2046d29b9ca69b0c576dcfa3a9f46c9d99f
Greg Kapka <greg@kapka.co.uk>
2018-06-09 18:42:25 +0100

    test that paid for FreeLOT onBehalfOf entries inc prizepool correctly

--- test/testEtheraffleTicketPurchasing.js
+++ test/testEtheraffleTicketPurchasing.js
@@ -493,6 +493,32 @@ contract('EtheraffleTicketPurchasing', accounts => {
     assert.equal(args.forRaffle.toNumber(), week)
   })
 
+  it('FreeLOT on behalf of entries with payment increment prize pool correctly', async () => {
+    const contract        = await etheraffle.deployed()
+        , freeCont        = await freeLOT.deployed()
+        , week            = await contract.getWeek.call()
+        , numbers         = [7,8,9,10,11,12]
+        , tktPrice        = 1*10**17
+        , affID           = 0
+        , buyer           = accounts[0]
+        , onBehalfOf      = accounts[3]
+        , freeBal         = await freeCont.balanceOf.call(buyer)
+        , prizePoolBefore = await contract.prizePool.call()
+    assert.isAbove(freeBal.toNumber(), 0, 'FreeLOT balance is zero!')
+    const entry = await contract.enterOnBehalfOfFree(numbers, affID, onBehalfOf, {from: buyer, value: tktPrice})
+    truffleAssert.eventEmitted(entry, 'LogTicketBought', ev =>
+      ev.tktCost.toNumber() == tktPrice &&
+      ev.chosenNumbers.reduce((acc, e, i) => acc && e == numbers[i], true)
+    )
+    /* Can't access indexed logs via truffleAssert hence following */
+    const [{ args }]     = await getAllEvents(etheraffle.at(contract.address))
+        , numEntries     = await contract.getUserNumEntries(onBehalfOf, week)
+        , prizePoolAfter = await contract.prizePool.call()
+    assert.equal(args.theEntrant, onBehalfOf)
+    assert.equal(args.personalEntryNumber.toNumber(), numEntries.toNumber())
+    assert.equal(args.forRaffle.toNumber(), week)
+    assert.equal(prizePoolAfter.toNumber(), prizePoolBefore.toNumber() + tktPrice)
+  })
 
   // it('FreeLOT on behalf of increments free entries & entries correctly', async () => {})
   // free entries that user pays for increments prizepool correctly! (for normal free and behalf of free!)
