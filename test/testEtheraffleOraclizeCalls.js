593ec6ea2761e977fcd0570ce91082a26df4bc08
Greg Kapka <greg@kapka.co.uk>
2018-06-11 23:04:14 +0100

    test setting Orac strings using the test versions of API calls

--- test/testEtheraffleOraclizeCalls.js
+++ test/testEtheraffleOraclizeCalls.js
@@ -25,4 +25,24 @@ contract('Etheraffle Oraclize Tests', accounts => {
       // Transaction reverts as expected!
     }
   })
+
+  it('Owner can set Oraclize strings correctly', async () => {
+    // Get owner -> change strings as owner -> check they match.
+    const contract = await etheraffle.deployed()
+        , owner    = await contract.etheraffle.call()
+        , random1  = "[URL] ['json(https://api.random.org/json-rpc/1/invoke).result.random[\"data\", \"serialNumber\"]','\\n{\"jsonrpc\": \"2.0\",\"method\":\"generateSignedIntegers\",\"id\":\""
+        , random2  = "\",\"params\":{\"n\":\"6\",\"min\":1,\"max\":49,\"replacement\":false,\"base\":10,\"apiKey\":${[decrypt] BBxn5oQTs8LKRkJb32LS+dHf/c//H3sSjehJchlucpdFGEjBwtSu08okSPoSkoQQpPCW56kz7PoGm5VEc8r722oEg01AdB03CbURpSxU5cF9Q7MeyNAaDUcTOvlX1L2T/h/k4PUD6FEIvtynHZrSMisEF+r7WJxgiA==}}']"
+        , api1     = "[URL] ['json(https://etheraffle.com/api/test).m','{\"r\":\""
+        , api2     = "\",\"k\":${[decrypt] BEhjzZIYd3GIvFUu4rWqwYOFKucnwToOUpP3x/svZVz/Vo68c6yIiq8k6XQDmPLajzSTD/TrpR5cF4BnLLhNDtELy7hQyMmFTuUa3JXBs0G0f4d7cTeIX8IG37KxtNfcvUafJy25}}']"
+    await contract.manuallySetOraclizeString(random1, random2, api1, api2, {from: owner})
+    const random1After = await contract.randomStr1.call()
+        , random2After = await contract.randomStr2.call()
+        , api1After    = await contract.apiStr1.call()
+        , api2After    = await contract.apiStr2.call()
+    assert.equal(random1, random1After, 'Random1 string not set correctly!')
+    assert.equal(random2, random2After, 'Random2 string not set correctly!')
+    assert.equal(api1, api1After, 'Api1 string not set correctly!')
+    assert.equal(api2, api2After, 'Api2 string not set correctly!')
+  })
+
 })
