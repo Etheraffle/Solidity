b074691b849ee700773690d1c85fea71fb019194
Greg Kapka <greg@kapka.co.uk>
2018-06-16 19:01:54 +0100

    check non-owners can't make Oraclize calls

--- test/testEtheraffleOraclizeCalls.js
+++ test/testEtheraffleOraclizeCalls.js
@@ -14,31 +14,39 @@ const { assert }    = require("chai")
 contract('Etheraffle Oraclize Tests', accounts => {
 
   it('Contract should have prize pool of 1 ETH', async () => {
-    // Add 1 ETH to prize pool -> query prize pool -> assert that it's 1ETH
+    // Add 1 ETH to prize pool -> query prize pool -> assert that it's 1ETH.
     const contract  = await etheraffle.deployed()
         , amount    = 1*10**18
-    await contract.manuallyAddToPrizePool({from: account[6], value: amount})
+    await contract.manuallyAddToPrizePool({from: accounts[6], value: amount})
     const prizePool = await contract.prizePool.call()
     assert.equal(prizePool.toNumber(), amount, 'Prize pool is not 1 ETH!')
   })
   
   it('Non-owner cannot set Oraclize strings', async () => {
-    // Change string as non-owner -> Check tx fails.
+    // Change string as non-owner -> Check tx fails -> Check strings weren't changed.
     const contract = await etheraffle.deployed()
         , owner    = await contract.etheraffle.call()
-        , random1  = 'Only'
-        , random2  = 'owner'
-        , api1     = 'can'
-        , api2     = 'set'
+        , r1       = 'Only'
+        , r2       = 'owner'
+        , a1       = 'can'
+        , a2       = 'set'
         , caller   = accounts[5]
     assert.notEqual(owner, caller, 'Function caller is same address contract owner!')
     try {
-      await contract.manuallySetOraclizeString(random1, random2, api1, api2, {from: caller})
+      await contract.manuallySetOraclizeString(r1, r2, a1, a2, {from: caller})
       assert.fail('Only contract owner should be able to set Oraclize strings!')
     } catch (e) {
       // console.log('Error when non-owner attempts to set Oraclize strings: ', e)
       // Transaction reverts as expected!
     }
+    const r1After = await contract.randomStr1.call()
+        , r2After = await contract.randomStr2.call()
+        , a1After = await contract.apiStr1.call()
+        , a2After = await contract.apiStr2.call()
+    assert.notEqual(r1, r1After, 'Random1 string was changed and shouldn\'t have been!')
+    assert.notEqual(r2, r2After, 'Random2 string was changed and shouldn\'t have been!')
+    assert.notEqual(a1, a1After, 'Api1 string was changed and shouldn\'t have been!')
+    assert.notEqual(a2, a2After, 'Api2 string was changed and shouldn\'t have been!')
   })
 
   it('Owner can set Oraclize strings', async () => {
@@ -54,6 +62,24 @@ contract('Etheraffle Oraclize Tests', accounts => {
     assert.equal(random2, random2After, 'Random2 string was not set correctly!')
     assert.equal(api1, api1After, 'Api1 string was not set correctly!')
     assert.equal(api2, api2After, 'Api2 string was not set correctly!')
+  })
+
+  it('Non-owner cannot make a manual Oraclize query', async () => {
+    const contract = await etheraffle.deployed()
+        , owner = await contract.etheraffle.call()
+        , guineaPig = accounts[5]
+        , week = 1
+        , delay = 0
+        , random = false
+        , manual = true
+        , status = false
+    assert.notEqual(owner, guineaPig, 'Guinea pig account is the same as owner!')
+    try {
+      await contract.manuallyMakeOraclizeCall(week, delay, random, manual, status, {from: guineaPig})
+    } catch (e) {
+      // console.log('Error when non-owner attempts to make Oraclize query: ', e)
+      // Transaction reverts as expected!
+    }
   })
   //Check callbacks don't work when paused
   //Check qid structs are made correctly for both types
